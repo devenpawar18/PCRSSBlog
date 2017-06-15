0dbd6fa1211986d1d3b3317e43ce6588cd558a65
Deven Bhagwan Pawar <dev.pawar.edu@gmail.com>
2017-06-14 17:30:53 -0700

    Initial project creation.

---
+++ app/src/main/java/com/pcrssblog/ArticleListActivity.java
@@ -0,0 +1,12 @@
+package com.pcrssblog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArticleListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
